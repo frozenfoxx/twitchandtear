997c3076a364e086e264d5db377b65a4ff4746e3
FrozenFOXX <frozenfoxx@churchoffoxx.net>
2021-12-31 10:09:15 +0000

    Deleted old Ruby class

--- lib/gameevent.rb
+++
@@ -1,9 +0,0 @@
-# GameEvent is the base class that defines communication to and from a game in progress
-class GameEvent
-  def intialize
-    # List of valid commands
-    @commands = Array.new
-  end
-
-  attr_reader :commands
-end
