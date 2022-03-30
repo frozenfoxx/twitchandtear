bf9dd8349d37c7ba0acc6fe1228b05a24465991c
FrozenFOXX <frozenfoxx@churchoffoxx.net>
2022-03-30 07:03:39 +0000

    Updated auth provider

--- twitchandtear/twitch/index.js
+++ twitchandtear/twitch/index.js
@@ -1,5 +1,8 @@
 'use strict'
 
+import { ClientCredentialsAuthProvider } from '@twurple/auth';
+
+
 // required libraries
 const RefreshingAuthProvider = require('@twurple/auth').RefreshingAuthProvider
 const ChatClient = require('@twurple/chat').ChatClient
@@ -12,15 +15,7 @@ const client_id = process.env.CLIENT_ID || ''
 const client_secret = process.env.CLIENT_SECRET || ''
 
 // set up authorization for Twitch
-const tokenData = JSON.parse(await fs.readFile('./tokens.json', 'UTF-8'))
-const authProvider = new RefreshingAuthProvider(
-	{
-		client_id,
-		client_secret,
-		onRefresh: async newTokenData => await fs.writeFile('./tokens.json', JSON.stringify(newTokenData, null, 4), 'UTF-8')
-	},
-	tokenData
-)
+const authProvider = new ClientCredentialsAuthProvider(client_id, client_secret)
 
 // connect to the chat
 const twitchChatClient = new ChatClient({ authProvider, channels: channels })
